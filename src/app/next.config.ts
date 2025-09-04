99634d06c86960e0d508526922fdf2dd1f579162
Stranger's Things <strangerssthinggame@gmail.com>
2025-09-04 20:51:24 +0000

    ⚠  apphosting: Rollout for backend studio failed. ⬢  apphosting:Failed t

---
+++ src/app/next.config.ts
@@ -0,0 +1,23 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
