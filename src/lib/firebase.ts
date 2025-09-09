e50e7b9c00c7b8bb1eeb1c23fb40de975a342367
Stranger's Things <strangerssthinggame@gmail.com>
2025-09-09 15:13:16 +0000

    gs://vibeverse-nk9c7.firebasestorage.app
    connect Storage

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,9 @@
+
 // src/lib/firebase.ts
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5NdNhtd5YtgPRRbbX5bBBNUz-PyxgBOo",
@@ -16,6 +18,7 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
 const googleAuthProvider = new GoogleAuthProvider();
 
-export { app, auth, firestore, googleAuthProvider };
+export { app, auth, firestore, storage, googleAuthProvider };
