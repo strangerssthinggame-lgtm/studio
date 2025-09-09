f3f91ed15a0b49178a2e3d271336cd8e43ab9eca
Stranger's Things <strangerssthinggame@gmail.com>
2025-09-09 12:56:38 +0000

    apiKey: "AIzaSyD5NdNhtd5YtgPRRbbX5bBBNUz-PyxgBOo",   authDomain: "vibeve

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Replace with your project's firebaseConfig object
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE"
+  apiKey: "AIzaSyD5NdNhtd5YtgPRRbbX5bBBNUz-PyxgBOo",
+  authDomain: "vibeverse-nk9c7.firebaseapp.com",
+  projectId: "vibeverse-nk9c7",
+  storageBucket: "vibeverse-nk9c7.appspot.com",
+  messagingSenderId: "736300805535",
+  appId: "1:736300805535:web:d55daebffbf44f3d2261aa"
 };
 
 // Initialize Firebase
